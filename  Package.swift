ac30923a37796a72da0c9b83b506342929976c28
Makoto Kinoshita <mkino@hmdt.jp>
2024-12-04 15:46:49 +0900

    フォントの追加

---
+++  Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "stone_engine",
+    platforms: [
+        .iOS(.v13), .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "stone_engine",
+            targets: ["stone_engine"]
+        ),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "stone_engine",
+            dependencies: []
+        ),
+    ]
+)
--- sample/AppDelegate.swift
+++ sample/AppDelegate.swift
@@ -7,14 +7,54 @@ Copyright 2024 Nihon Design Center. All rights reserved.
 This software is licensed under the MIT License. See LICENSE for details.
 */
 
+import CoreText
 import UIKit
+
+let additionalFonts: [String] = [
+    "YuMin-Medium", "YuMin-Demibold", "YuMin-Extrabold", "YuGo-Medium", "YuGo-Bold",
+]
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Match fonts
+        matchFont(withNames: additionalFonts) { result in
+        }
+        
+        return true
+    }
+}
+
+func matchFont(withNames names: [String], completion: ((Bool) -> Void)?) {
+    // Create font descriptors
+    var fontDescriptors: [CTFontDescriptor] = []
+    for name in names {
+        let fontDescriptor = CTFontDescriptorCreateWithAttributes([kCTFontNameAttribute as NSAttributedString.Key: name] as CFDictionary)
+        fontDescriptors.append(fontDescriptor)
+    }
+    
+    // Match font descriptors
+    CTFontDescriptorMatchFontDescriptorsWithProgressHandler(fontDescriptors as CFArray, nil) { state, progressParameter in
+        // Switch by state
+        switch state {
+        case .didFinish:
+            // Dispatch to main
+            DispatchQueue.main.async {
+                // Invoke handler
+                completion?(true)
+            }
+        case .didFailWithError:
+            // Dispatch to main
+            DispatchQueue.main.async {
+                // Invoke handler
+                completion?(false)
+            }
+        default:
+            break
+        }
+        
         return true
     }
 }
